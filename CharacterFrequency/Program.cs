ca0ff1d37c46182002c731607a2ee2cf793adbef
Harshal Chaudhari <harshalschaudhari@gmail.com>
2025-04-03 22:18:15 +0530

    C# solution for CharacterFrequency

--- CharacterFrequency/Program.cs
+++ CharacterFrequency/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CharacterFrequency
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string word = "Mississippi";
+            char ch = 's';
+            int frequency = returnCharacterFrequency(word, ch);
+            Console.WriteLine($"Word: '{word}' has the character '{ch}' with a frequency of {frequency}.");
+            Console.ReadKey();
+        }
+
+        public static int returnCharacterFrequency(string word, char ch)
+        {
+            int frequency = 0;
+            for(int index = 0; index < word.Length; index++)
+            {
+                if(word[index] == ch)
+                {
+                    frequency++;
+                }
+            }
+            return frequency;
+        }
+    }
+}
